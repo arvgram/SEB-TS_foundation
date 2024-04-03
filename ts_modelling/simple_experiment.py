e737efb439dc343effd92752b6d2a14fce0766f1
arvgram <arvid.gramer@gmail.com>
2024-04-03 17:40:15 +0200

    add pretrain-train-finetune pipeline

--- ts_modelling/simple_experiment.py
+++ ts_modelling/simple_experiment.py
@@ -9,8 +9,9 @@ from PatchTST.PatchTST_supervised.exp.exp_basic import Exp_Basic
 from PatchTST.PatchTST_supervised.data_provider.data_factory import data_provider
 from PatchTST.PatchTST_supervised.models import PatchTST
 from PatchTST.PatchTST_supervised.utils.tools import EarlyStopping
+from PatchTST.PatchTST_supervised.layers.PatchTST_backbone import Flatten_Head
+from model_components import PretrainHead
 
-from torch import load
 from torch import optim
 from torch import nn
 
@@ -56,6 +57,13 @@ class SimpleExp(Exp_Basic):
             'PatchTST': PatchTST,
         }
         model = model_dict[self.args.model].Model(self.args).float()
+        self.num_patches = int((self.args.seq_len - self.args.patch_len) / self.args.stride + 1)
+
+        if self.args.training_task == 'self_supervised':
+            model = self._swap_head(model=model, new_head='self_supervised')
+            print('self_supervised head')
+        elif self.args.training_task == 'supervised':
+            print('supervised head')
         return model
 
     def infer_and_get_loss(self, batch_x, batch_y, model, criterion, task):
@@ -68,18 +76,17 @@ class SimpleExp(Exp_Basic):
             loss = criterion(pred, true)
 
         elif task == 'self_supervised':
-            # if self supervised:
-            #   draw random masked_indices of batch_x,
-            #   predict using masked batch_x elementwise multiplied with NOT masked
-            #   extract values at masked_indices from batch_x and output
-            #   calculate loss as mse between output and unmasked_batch_x at indices
+            """if self supervised:
+              draw random masked_indices of batch_x,
+              predict using masked batch_x elementwise multiplied with NOT masked
+              extract values at masked_indices from batch_x and output
+              calculate loss as mse between output and unmasked_batch_x at indices"""
 
             # unmasked_batch_x = batch_x.clone()
-            num_patches = int((self.args.seq_len - self.args.patch_len) / self.args.stride + 1)
-            num_indices = int(self.args.mask_pct * num_patches)
+            num_indices = int(self.args.mask_pct * self.num_patches)
             mask_indices_mtx = torch.zeros_like(batch_x, dtype=torch.bool)
             for b in range(batch_x.shape[0]):
-                mask_start_indices = sorted(random.sample(range(num_patches), num_indices))
+                mask_start_indices = sorted(random.sample(range(self.num_patches), num_indices))
                 mask_indices = []
                 for index in mask_start_indices:
                     start = index * self.args.stride
@@ -96,14 +103,52 @@ class SimpleExp(Exp_Basic):
 
         return loss
 
-    def train(self):
+    def swap_head(self, new_head):
+        """Changes head on model to type corresponding to new_head
+        """
+
+        self.model = self._swap_head(model=self.model, new_head=new_head)
+
+    def _swap_head(self, model, new_head):
+        if new_head == 'supervised':
+            model.model.head = Flatten_Head(
+                individual=self.args.individual,
+                n_vars=self.args.enc_in,
+                nf=self.args.d_model*(self.num_patches+1),
+                target_window=self.args.pred_len,
+                head_dropout=self.args.head_dropout
+            )
+
+        elif new_head == 'self_supervised':
+            model.model.head = PretrainHead(
+                d_model=self.args.d_model,
+                patch_len=self.args.patch_len,
+                dropout=self.args.dropout
+            )
+
+        return model
+
+    def freeze_backbone(self, freeze=True):
+        for param in self.model.model.backbone.parameters():
+            param.requires_grad = not freeze
+
+    def train(self, n_epochs=None):
+        if n_epochs is None:
+            n_epochs = self.args.train_epochs
+
+        if self.args.verbose:
+            total_params = sum(p.numel() for p in self.model.parameters())
+            trainable_params = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
+            print(f'Trainable parameters:\t{trainable_params}')
+            print(f'Total parameters:\t\t{total_params}')
+
         tot_time_start = time.time()
 
         train_data, train_loader = self._get_data(flag='train')
         val_data, val_loader = self._get_data(flag='val')
         test_data, test_loader = self._get_data(flag='test')
 
-        save_path = os.path.join(self.args.checkpoints, self.args.model_name)
+        save_path = os.path.join(self.args.checkpoints, self.args.model_name, self.args.training_task)
         if not os.path.exists(save_path):
             os.makedirs(save_path)
 
@@ -117,7 +162,7 @@ class SimpleExp(Exp_Basic):
         criterion = self._select_criterion()
         scheduler = self._select_lr_scheduler(optimiser, train_steps)
 
-        for epoch in range(self.args.train_epochs):
+        for epoch in range(n_epochs):
             epoch_time_start = time.time()
             train_loss = []
 
@@ -139,13 +184,6 @@ class SimpleExp(Exp_Basic):
                     task=self.args.training_task,
                 )
 
-                # outputs = self.model(batch_x)
-                #
-                # f_dim = -1 if self.args.features == 'MS' else 0
-                # outputs = outputs[:, -self.args.pred_len:, f_dim:]
-                # batch_y = batch_y[:, -self.args.pred_len:, f_dim:]
-                # loss = criterion(outputs, batch_y)
-
                 train_loss.append(loss.item())
 
                 if (i + 1) % self.args.batch_log_interval == 0:
@@ -162,9 +200,9 @@ class SimpleExp(Exp_Basic):
             if (epoch + 1) % self.args.epoch_log_interval == 0:
                 print(
                     f'epoch: {epoch + 1}, '
-                    f'train loss: {train_loss:.7}, '
-                    f'validation loss: {val_loss:}, '
-                    f'test loss: {test_loss}'
+                    f'train loss: {train_loss:.5}, '
+                    f'validation loss: {val_loss:.5}, '
+                    f'test loss: {test_loss:.5}'
                 )
                 print(
                     f'epoch time: {time.time() - epoch_time_start}'
@@ -176,14 +214,14 @@ class SimpleExp(Exp_Basic):
             if self.args.verbose:
                 print('Updating learning rate to {}'.format(scheduler.get_last_lr()[0]))
 
-        best_model = self.model.load_state_dict(load(os.path.join(save_path, 'checkpoint.pth')))
+        self.model.load_state_dict(torch.load(os.path.join(save_path, 'checkpoint.pth')))
 
         total_training_time = time.time() - tot_time_start
         minutes = total_training_time // 60
         seconds = total_training_time % 60
 
         print(f'Total training time: {int(minutes)} minutes {seconds} seconds')
-        return best_model
+        return self.model
 
     def validate(self, val_loader, criterion):
         total_loss = []
@@ -207,6 +245,31 @@ class SimpleExp(Exp_Basic):
         self.model.train()
         return np.average(total_loss)
 
+    def pretrain_model(self, n_epochs=None, task='self_supervised'):
+        """If needed attaches pretrain head and trains on self_supervised setting"""
+        if n_epochs is None:
+            n_epochs = self.args.pretrain_epochs
+        if not isinstance(self.model.model.head, PretrainHead):
+            self.swap_head(new_head=task)
+        self.train(n_epochs=n_epochs)
+
+    def train_predict_head(self, n_epochs=None, task='supervised'):
+        """if needed attaches prediction head and trains only head with frozen backbone"""
+        if n_epochs is None:
+            n_epochs = self.args.train_head_epochs
+        if not isinstance(self.model.model.head, Flatten_Head):
+            self.swap_head(new_head=task)
+        self.args.training_task = 'supervised'
+        self.freeze_backbone(freeze=True)
+        self.train(n_epochs=n_epochs)
+
+    def finetune_model(self, n_epochs=None):
+        """unfreezes all parameters and trains"""
+        if n_epochs is None:
+            n_epochs = self.args.finetune_epochs
+        self.freeze_backbone(freeze=False)
+        self.train(n_epochs=n_epochs)
+
     def test(self):
         """
         data can be path to dataset or numpy array. If unspecified tests on test chunk of training data
@@ -218,19 +281,3 @@ class SimpleExp(Exp_Basic):
     def predict(self):
         pass
 
-    def self_supervised_pretrain(self):
-        """
-        pretrains model backbone by attaching fill-in-patch head.
-        """
-        # get data
-
-        # setup save path
-
-        # setup early stopping
-
-        # setup
-
-    def train_predict_head(self):
-        """
-        attaches predict head and trains only head
-        """
